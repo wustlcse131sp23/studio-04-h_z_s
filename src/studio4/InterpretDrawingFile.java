5d39261e72f681c52349cf286a5f5d9f7d8a50a7
kaiyuky <kaiyu@wustl.edu>
2023-02-23 11:56:17 -0600

    task one finished!

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,8 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -19,6 +21,27 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isfilled = in.nextBoolean();
+		double paraOne = in.nextDouble();
+		double paraTwo = in.nextDouble();
+		double paraThree = in.nextDouble();
+		double paraFour = in.nextDouble();
+		
+		Color color = new Color(redComponent, greenComponent, blueComponent);
+		StdDraw.setPenColor(color);
+		if (isfilled) {
+			StdDraw.filledRectangle(paraOne,paraTwo,paraThree,paraFour);
+		}else
+		{
+			StdDraw.rectangle(paraOne,paraTwo,paraThree,paraFour);
+		}
+		
 		
 	}
 }
